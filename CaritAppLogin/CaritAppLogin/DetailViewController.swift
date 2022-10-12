aa76eaec6e66e5f58f96b6ff6ce9fe09d4e858d2
Alberto Estrada <a01276671@itesm.mx>
2022-10-12 10:59:58 -0500

    g

--- CaritAppLogin/CaritAppLogin/DetailViewController.swift
+++ CaritAppLogin/CaritAppLogin/DetailViewController.swift
@@ -15,6 +15,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var project: UILabel!
     @IBOutlet weak var informacion: UILabel!
     
+    @IBOutlet weak var buttonElegirHorario: UIButton!
+    
     var tituloMostrar: String!
     var imagenMostrar: String!
     var desc: String!
@@ -26,9 +28,23 @@ class DetailViewController: UIViewController {
         project.text = tituloMostrar
         image.image = UIImage(named: imagenMostrar)
         informacion.text = desc
+        setPopUpButton()
         
         background.layer.cornerRadius = 24
         // Do any additional setup after loading the view.
+    }
+    
+    func setPopUpButton(){
+        let optionClosure = {(action : UIAction) in
+            print(action.title)}
+        
+        buttonElegirHorario.menu = UIMenu(children : [
+            UIAction(title : "Horario", state : .on, handler: optionClosure),
+            UIAction(title : "Horario Matutino", handler: optionClosure),
+            UIAction(title : "Horario Despertino", handler: optionClosure)])
+        
+        buttonElegirHorario.showsMenuAsPrimaryAction = true
+        buttonElegirHorario.changesSelectionAsPrimaryAction = true
     }
     
     
